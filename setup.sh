958f8bf2af0098149490169a69aa980194a5fb77
Frans-Lukas Lövenvald <franslukas@gmail.com>
2021-03-23 15:01:34 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 cat bashrc >> ~/.bashrc 
 soruce ~/.bashrc
+
+copyMacros(){
+        for dir in ~/.config/JetBrains/*; do
+                read -p "copying macros.xml to $dir/options/macros.xml, y/n?" yn
+                case $yn in
+                    [Yy]* ) break;;
+                    [Nn]* ) return;;
+                    * ) echo "Please answer yes or no.";;
+                esac
+                cp macros.xml $dir/options/macros.xml
+        done
+}
+
+copyMacros
