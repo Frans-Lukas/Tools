a2acb04d087efb104af2f83a7cc2a17c05ec9ca8
Frans-Lukas <franslukas@pm.me>
2021-03-26 11:37:06 +0100

    creates keymaps dir and works for all jetbrains now

--- setup.sh
+++ setup.sh
@@ -6,21 +6,21 @@ copyMacros(){
         for dir in ~/.config/JetBrains/*; do
                 read -p "copying macros.xml to $dir/options/macros.xml, y/n?" yn
                 case $yn in
-                    [Yy]* ) break;;
+                    [Yy]* ) cp macros.xml $dir/options/macros.xml;;
                     [Nn]* ) return;;
                     * ) echo "Please answer y or n.";;
                 esac
-                cp macros.xml $dir/options/macros.xml
         done
         
         for dir in ~/.config/JetBrains/*; do
                 read -p "copying my_keybinds.xml to $dir/keymaps/my_keybinds.xml, y/n?" yn
                 case $yn in
-                    [Yy]* ) break;;
+                    [Yy]* ) 
+			    mkdir $dir/keymaps
+			    cp my_keybinds.xml $dir/keymaps/my_keybinds.xml;;
                     [Nn]* ) return;;
                     * ) echo "Please answer y or n.";;
                 esac
-                cp my_keybinds.xml $dir/keymaps/my_keybinds.xml
         done
 }
 
